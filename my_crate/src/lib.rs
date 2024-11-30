ae5f88971bfd8a2a1e9c92416bf30d4ee24c7a7c
s.v.kupriyanov <s.v.kupriyanov@tinkoff.ru>
2024-11-30 22:25:04 +0400

    Create my first crate to publish to crates.io

---
+++ my_crate/src/lib.rs
@@ -0,0 +1,28 @@
+//! # My Crate
+//!
+//! `simple-one-adder` is a library that helps you add one to different numbers
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add_one(3);
+        assert_eq!(result, 4);
+    }
+}
