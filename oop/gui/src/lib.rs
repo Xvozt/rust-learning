f10a96e78b051fa183e41fd53bb5e4a477064fd7
Sergey Kupriyanov <sergeykupriyanov@Sergeys-MacBook-Pro.local>
2024-12-23 22:39:12 +0400

    trait objects topic is finished with notes

---
+++ oop/gui/src/lib.rs
@@ -0,0 +1,38 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>, // trait object, the exact type is known at runtime using vtable
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+pub struct TextField {
+    pub placeholder: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // Implementation for drawing a button
+        println!("Drawing a button: {}", self.label);
+    }
+}
+
+impl Draw for TextField {
+    fn draw(&self) {
+        println!("Drawing a text field: {}", self.placeholder)
+    }
+}
