e6c1e9530a7885b39da7f856b5a8c55b0fb0dfba
Sergey Kupriyanov <sergeykupriyanov@Sergeys-MacBook-Pro.local>
2024-12-25 00:20:32 +0400

    example of state pattern implementation

--- oop/draw/src/main.rs
+++ oop/draw/src/main.rs
@@ -1,3 +1,4 @@
+use blog::Post;
 use gui::{Button, Draw, Screen};
 
 struct SelectBox {
@@ -33,4 +34,15 @@ fn main() {
     };
 
     screen.run();
+
+    let mut post = Post::new();
+
+    post.add_text("I ate salad for lunch today");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("I ate salad for lunch today", post.content());
 }
