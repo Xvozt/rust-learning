25fccda391a9e6dd756e9946dbc92d7b7c0d5e7a
Sergey Kupriyanov <xvoztuk@gmail.com>
2025-01-06 00:36:20 +0400

    simple web server

---
+++ hello_server/src/main.rs
@@ -0,0 +1,37 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let _http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let content = fs::read_to_string("hello.html").unwrap();
+    let length = content.len();
+
+    //Separate headers and content
+    let headers =
+        format!("{status_line}\r\nContent-Length: {length}\r\nContent-Type: text/html\r\n\r\n",);
+
+    stream.write_all(headers.as_bytes()).unwrap();
+    stream.write_all(content.as_bytes()).unwrap();
+
+    stream.flush().unwrap();
+}
